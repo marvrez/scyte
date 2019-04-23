3b65a8e78376629fa11d208321f03870e38316d5
Marvin Reza <marvinreza@outlook.com>
2019-04-24 00:01:25 +0200

    ignore .DS_Store and accomodate main.cpp for new tensor class

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
+#include "blas.h"
+#include "tensor.h"
+#include "utils.h"
+#include <cstdio>
+#include <math.h>
+
 int main(int argc, char** argv)
 {
+    int M = 300, N = 300, K = 300;
+    tensor a  = make_random_uniform_tensor(M, K);
+    tensor b = make_random_uniform_tensor(K, N);
+    tensor c = make_2d_tensor(M, N);
+
+    double t1 = time_now();
+    gemm_cpu(false, false, M, N, K, 1, a.data, b.data, 1, c.data);
+    double t2 = time_now();
+    printf("time taken: %.6lf miliseconds\n", (t2-t1)*100);
     return 0;
 }
