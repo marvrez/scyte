30e96e8bee0ac760501b4efac177671bfa6680fe
Marvin Reza <marvinreza@outlook.com>
2019-05-11 00:41:22 +0200

    implement add op

---
+++ include/ops/add.h
@@ -0,0 +1,11 @@
+#ifndef ADD_H
+#define ADD_H
+
+#include "scyte.h"
+
+scyte_node* scyte_add(scyte_node* x, scyte_node* y);
+
+void scyte_add_forward(scyte_node* node);
+void scyte_add_backward(scyte_node* node);
+
+#endif
