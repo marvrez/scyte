ca61a82b214a1a2c62261314a648fa30824d6ff5
Marvin Reza <marvinreza@outlook.com>
2019-06-13 12:55:42 +0200

    implement doubly linked list

---
+++ include/list.h
@@ -0,0 +1,29 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct list_node {
+    void* data;
+    struct list_node* next;
+    struct list_node* prev;
+} list_node;
+
+typedef struct list {
+    int size;
+    list_node* head;
+    list_node* tail;
+} list;
+
+list* make_list();
+int list_find(list *l, void *val);
+
+void list_append(list*, void*);
+void list_prepend(list*, void*);
+
+void* list_pop(list*);
+
+void free_list_contents(list *l);
+void** list_to_array(list *l);
+void** list_to_reverse_array(list *l);
+void free_list(list *l);
+
+#endif
---
+++ src/list.c
@@ -0,0 +1,105 @@
+#include "list.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+list* make_list()
+{
+    list* l = malloc(sizeof(list));
+    l->size = 0;
+    l->head = NULL;
+    l->tail = NULL;
+    return l;
+}
+
+void* list_pop(list* l) 
+{
+    if(!l->head) return NULL;
+    list_node* n = l->head;
+    void* data = n->data;
+    l->head = n->next;
+    if(l->head) l->head->prev = NULL;
+
+    free(n);
+    --l->size;
+    
+    return data;
+}
+
+void list_prepend(list* l, void* data)
+{
+    list_node* new = malloc(sizeof(list_node));
+    new->data = data, new->next = new->prev = NULL;
+    if(l->head) l->head->prev = new;
+    new->next = l->head;
+    l->head = new;
+    if(!l->tail) l->tail = l->head;
+    ++l->size;
+}
+
+void list_append(list* l, void* data)
+{
+    list_node* new = malloc(sizeof(list_node));
+    new->data = data, new->next = NULL;
+
+    if(!l->tail) {
+        l->head = new;
+        new->prev = NULL;
+    }
+    else {
+        l->tail->next = new;
+        new->prev = l->tail;
+    }
+    l->tail = new;
+    ++l->size;
+}
+
+void free_node(list_node* n)
+{
+    list_node* next;
+    while(n) {
+        next = n->next;
+        free(n);
+        n = next;
+    }
+}
+
+void free_list(list* l)
+{
+    free_node(l->head);
+    free(l);
+}
+
+void free_list_contents(list* l)
+{
+    list_node* n = l->head;
+    while(n) {
+        free(n->data);
+        n = n->next;
+    }
+}
+
+void** list_to_array(list* l)
+{
+    void** a = calloc(l->size, sizeof(void*));
+    int count = 0;
+    list_node* n = l->head;
+    while(n) {
+        a[count++] = n->data;
+        n = n->next;
+    }
+    return a;
+}
+
+void** list_to_reverse_array(list* l)
+{
+    void** a = calloc(l->size, sizeof(void*));
+    int count = 0;
+    list_node* n = l->tail;
+    while(n) {
+        a[count++] = n->data;
+        n = n->prev;
+    }
+    return a;
+}
