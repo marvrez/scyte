5ae0e149b83cfabb76b0383db2fc1994f12e0634
Marvin Reza <marvinreza@outlook.com>
2019-12-11 00:01:01 +0100

    split optimizer steps into different functions

--- include/optimizer.h
+++ include/optimizer.h
@@ -13,13 +13,15 @@ typedef struct {
     float decay; // regularization
     float momentum;
     float alpha; // for RMSProp
-    float beta_1, beta_2; // for adam, exponential averaging
+    float beta1, beta2; // for adam, exponential averaging
 } scyte_optimizer_params;
 
-scyte_optimizer_params scyte_adam_params(float lr, float decay, float momentum, float beta_1, float beta_2);
+scyte_optimizer_params scyte_adam_params(float lr, float decay, float momentum, float beta1, float beta2);
 scyte_optimizer_params scyte_rmsprop_params(float lr, float decay, float momentum, float alpha);
 scyte_optimizer_params scyte_sgd_params(float lr, float decay, float momentum);
 
-void scyte_optimizer_step(scyte_optimizer_params params, int n, const float* g, float* g_prev, float* w);
+void scyte_sgd_step(scyte_optimizer_params params, int n, const float* g, float* g_prev, float* w);
+void scyte_rmsprop_step(scyte_optimizer_params params, int n, const float* g, float* g_var, float* w);
+void scyte_adam_step(scyte_optimizer_params params, int n, const float* g, float* g_var, float* g_mean, float* w);
 
 #endif
--- src/optimizer.c
+++ src/optimizer.c
@@ -1,14 +1,17 @@
 #include "optimizer.h"
 
 #include <math.h>
+#include <assert.h>
 
-scyte_optimizer_params scyte_adam_params(float lr, float decay, float momentum, float beta_1, float beta_2)
+#define EPS 1e-6f
+
+scyte_optimizer_params scyte_adam_params(float lr, float decay, float momentum, float beta1, float beta2)
 {
     scyte_optimizer_params p;
     p.type = ADAM;
     p.lr = lr;
     p.momentum = momentum;
-    p.beta_1 = beta_1, p.beta_2 = beta_2;
+    p.beta1 = beta1, p.beta2 = beta2;
     return p;
 }
 
@@ -31,34 +34,33 @@ scyte_optimizer_params scyte_sgd_params(float lr, float decay, float momentum)
     return p;
 }
 
-#define EPS 1e-6f
-
-void scyte_optimizer_step(scyte_optimizer_params params, int n, const float* g, float* g_prev, float* w)
+void scyte_sgd_step(scyte_optimizer_params params, int n, const float* g, float* g_prev, float* w)
 {
+    assert(params.type == SGD);
     float lr = params.lr, momentum = params.momentum;
-    if(params.type == SGD) {
-        for(int i = 0; i < n; ++i) {
-            g_prev[i] = -lr*g[i] + momentum*g_prev[i];
-            w[i] += g_prev[i];
-        }
+    for(int i = 0; i < n; ++i) {
+        g_prev[i] = -lr*g[i] + momentum*g_prev[i];
+        w[i] += g_prev[i];
     }
-    else if(params.type == RMSPROP) {
-        float alpha = params.alpha; 
-        for(int i = 0; i < n; ++i) {
-            g_prev[i] = alpha*g_prev[i] + (1.f - alpha)*g[i]*g[i];
-            w[i] += -lr*g[i]/sqrtf(g_prev[i] + EPS);
-        }
-    }
-    else if(params.type == ADAM) {
-        /*
-         * TODO: make a separate step function for each optimizer?
-        float beta1 = params.beta_1, beta2 = params.beta_2; 
-        for(int i = 0; i < n; ++i) {
-            g_mean[i] = beta1*g_prev[i] + (1.f - beta1)*g[i]; // estimate mean of gradient
-            g_var[i] = beta2*g_prev[i] + (1.f - beta2)*g[i]*g[i]; // estimate variance of gradients
-            w[i] += -lr*g_mean[i]/sqrtf(g_var[i] + EPS);
-        }
-        */
+}
 
+void scyte_rmsprop_step(scyte_optimizer_params params, int n, const float* g, float* g_var, float* w)
+{
+    assert(params.type == RMSPROP);
+    float lr = params.lr, alpha = params.alpha;
+    for(int i = 0; i < n; ++i) {
+        g_var[i] = alpha*g_var[i] + (1.f - alpha)*g[i]*g[i]; // estimate variance of gradients
+        w[i] += -lr*g[i]/sqrtf(g_var[i] + EPS);
+    }
+}
+
+void scyte_adam_step(scyte_optimizer_params params, int n, const float* g, float* g_var, float* g_mean, float* w)
+{
+    assert(params.type == ADAM);
+    float lr = params.lr, beta1 = params.beta1, beta2 = params.beta2;
+    for(int i = 0; i < n; ++i) {
+        g_mean[i] = beta1*g_mean[i] + (1.f - beta1)*g[i]; // estimate mean of gradient
+        g_var[i] = beta2*g_var[i] + (1.f - beta2)*g[i]*g[i]; // estimate variance of gradients
+        w[i] += -lr*g_mean[i]/sqrtf(g_var[i] + EPS);
     }
 }
