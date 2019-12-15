bc77913029d77c4da4872fec96a4fe55e30da032
Marvin Reza <marvinreza@outlook.com>
2019-12-15 22:11:34 +0100

    use decay parameter, and perform gradient descent instead of ascent

--- src/optimizer.c
+++ src/optimizer.c
@@ -37,10 +37,10 @@ scyte_optimizer_params scyte_sgd_params(float lr, float decay, float momentum)
 void scyte_sgd_step(scyte_optimizer_params params, int n, const float* g, float* g_prev, float* w)
 {
     assert(params.type == SGD);
-    float lr = params.lr, momentum = params.momentum;
+    float lr = params.lr, momentum = params.momentum, decay = params.decay;
     for(int i = 0; i < n; ++i) {
-        g_prev[i] = -lr*g[i] + momentum*g_prev[i];
-        w[i] += g_prev[i];
+        g_prev[i] = g[i] + momentum*g_prev[i] - decay*w[i];
+        w[i] -= lr*g_prev[i];
     }
 }
 
@@ -50,7 +50,7 @@ void scyte_rmsprop_step(scyte_optimizer_params params, int n, const float* g, fl
     float lr = params.lr, alpha = params.alpha;
     for(int i = 0; i < n; ++i) {
         g_var[i] = alpha*g_var[i] + (1.f - alpha)*g[i]*g[i]; // estimate variance of gradients
-        w[i] += -lr*g[i]/sqrtf(g_var[i] + EPS);
+        w[i] -= lr*(g[i]/sqrtf(g_var[i] + EPS) - decay*w[i]);
     }
 }
 
@@ -61,6 +61,6 @@ void scyte_adam_step(scyte_optimizer_params params, int n, const float* g, float
     for(int i = 0; i < n; ++i) {
         g_mean[i] = beta1*g_mean[i] + (1.f - beta1)*g[i]; // estimate mean of gradient
         g_var[i] = beta2*g_var[i] + (1.f - beta2)*g[i]*g[i]; // estimate variance of gradients
-        w[i] += -lr*g_mean[i]/sqrtf(g_var[i] + EPS);
+        w[i] -= lr*(g_mean[i]/sqrtf(g_var[i] + EPS) - decay*w[i]);
     }
 }
