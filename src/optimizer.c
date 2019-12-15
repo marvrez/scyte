5b6257629ce1ea41e2820ac5fab421137004aa2f
Marvin Reza <marvinreza@outlook.com>
2019-12-15 22:12:27 +0100

    initialize decay variable before using it

--- src/optimizer.c
+++ src/optimizer.c
@@ -47,7 +47,7 @@ void scyte_sgd_step(scyte_optimizer_params params, int n, const float* g, float*
 void scyte_rmsprop_step(scyte_optimizer_params params, int n, const float* g, float* g_var, float* w)
 {
     assert(params.type == RMSPROP);
-    float lr = params.lr, alpha = params.alpha;
+    float lr = params.lr, alpha = params.alpha, decay = params.decay;
     for(int i = 0; i < n; ++i) {
         g_var[i] = alpha*g_var[i] + (1.f - alpha)*g[i]*g[i]; // estimate variance of gradients
         w[i] -= lr*(g[i]/sqrtf(g_var[i] + EPS) - decay*w[i]);
@@ -57,7 +57,7 @@ void scyte_rmsprop_step(scyte_optimizer_params params, int n, const float* g, fl
 void scyte_adam_step(scyte_optimizer_params params, int n, const float* g, float* g_var, float* g_mean, float* w)
 {
     assert(params.type == ADAM);
-    float lr = params.lr, beta1 = params.beta1, beta2 = params.beta2;
+    float lr = params.lr, beta1 = params.beta1, beta2 = params.beta2, decay = params.decay;
     for(int i = 0; i < n; ++i) {
         g_mean[i] = beta1*g_mean[i] + (1.f - beta1)*g[i]; // estimate mean of gradient
         g_var[i] = beta2*g_var[i] + (1.f - beta2)*g[i]*g[i]; // estimate variance of gradients
